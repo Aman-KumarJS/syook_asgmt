c7d32060a352c868d851699e2a793b8ee2145924
Aman Kumar <67796620+Aman-KumarJS@users.noreply.github.com>
2021-03-13 21:58:59 +0530

    Update problem2.js

--- problem2.js
+++ problem2.js
@@ -1,5 +1,5 @@
 let digitalRoot = (n) => {
-  //calculate the sum of all the digits
+  
   let sum = 0;
   while(parseInt(n) > 0){
     let temp = n % 10;
@@ -7,7 +7,7 @@ let digitalRoot = (n) => {
     n = parseInt(n / 10);
   }
   
-  //Check if the sum is single digit
+ 
   if(sum < 10){
     return sum;
   }
@@ -16,7 +16,7 @@ let digitalRoot = (n) => {
   return digitalRoot(sum);
 }
 
-console.log(digitalRoot(257520643)); //7
-console.log(digitalRoot(5674));      // 4
-console.log(digitalRoot(493193));    // 2
-console.log(digitalRoot(34758));     // 9
+console.log(digitalRoot(257520643)); 
+console.log(digitalRoot(5674));      
+console.log(digitalRoot(493193));    
+console.log(digitalRoot(34758));     
